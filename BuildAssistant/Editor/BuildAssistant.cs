c8b7ba905d3e2c39a50203e98b87efb336473934
kazookie <kzki@hacklab.jp>
2023-04-08 11:10:24 +0900

    add BuildAssistant assets

---
+++ BuildAssistant/Editor/BuildAssistant.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+[InitializeOnLoad]
+public class BuildAssistant : MonoBehaviour
+{ 
+    static List<string> dependentPackages = new List<string>();
+    static List<string> importedPackages  = new List<string>();
+  
+    //
+    static BuildAssistant()
+    {    
+        string[] packages = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.unitypackage");
+        packages = packages.Select(f => Path.GetFileName(f)).ToArray();
+        dependentPackages =  new List<string>(packages);
+        
+        AssetDatabase.importPackageCompleted += OnImportPackageCompleted;
+    }
+
+    // 
+    static void ImportPackage()
+    {
+        foreach (string package in dependentPackages)
+        {
+            AssetDatabase.ImportPackage(package, false);
+        }
+    }
+    
+    //
+    private static void OnImportPackageCompleted(string packagename)
+    {
+        importedPackages.Add(packagename + ".unitypackage");
+        if (dependentPackages.SequenceEqual(importedPackages)){
+            EditorApplication.Exit(0);
+        }
+    }
+    
+    //
+    public static void Build()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/VirtualMotionCapture.unity" };
+        buildPlayerOptions.locationPathName = "UnityBuild/VirtualMotionCapture.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+        EditorApplication.Exit(0);
+    }
+}
